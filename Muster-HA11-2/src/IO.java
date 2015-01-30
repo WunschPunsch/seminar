a8e9e36b529a8b1f5bb2d6f57437dbcbc2dab8d4
kritzelbit <boerge.kiss@gmail.com>
2015-01-30 10:34:06 +0100

    added readFile() method to IO

--- Muster-HA11-2/src/IO.java
+++ Muster-HA11-2/src/IO.java
@@ -1,12 +1,26 @@
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 
+/**
+ * Ein- und Ausgabe für HA11-2
+ * @author bkis
+ *
+ */
 public class IO {
 	
+	/**
+	 * Datei schreiben
+	 * @param content
+	 * @param path
+	 * @throws IOException
+	 */
 	public void writeFile(String content, String path) throws IOException{
 		OutputStream os = new FileOutputStream(new File(path));
 		BufferedOutputStream bos = new BufferedOutputStream(os);
@@ -16,6 +30,26 @@ public class IO {
 		}
 		
 		bos.close();
+	}
+	
+	/**
+	 * Datei lesen
+	 * @param path
+	 * @return
+	 * @throws IOException
+	 */
+	public String readFile(String path) throws IOException{
+		InputStream is = new FileInputStream(new File(path));
+		BufferedInputStream bis = new BufferedInputStream(is);
+		StringBuffer sb = new StringBuffer();
+		
+		int i;
+		while ((i = bis.read()) != -1){
+			sb.append((char)i);
+		}
+		
+		bis.close();
+		return sb.toString();
 	}
 
 }
